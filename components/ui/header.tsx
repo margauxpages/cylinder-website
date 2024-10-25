5d5e98c60c4b0e9a33cd4c0040e2fe4ab1e445d3
margauxpages <39771534+margauxpages@users.noreply.github.com>
2024-10-25 18:09:32 -0500

    Update header.tsx

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -17,18 +17,18 @@ export default function Header() {
           <ul className="flex flex-1 items-center justify-end gap-3">
             <li>
               <Link
-                href="/signin"
+                href="https://calendly.com/margauxpages-vh8/30min"
                 className="btn-sm relative bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] py-[5px] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%]"
               >
-                Sign In
+                Contact Us
               </Link>
             </li>
             <li>
               <Link
-                href="/signup"
+                href="https://www.linkedin.com/company/tma-hazardous-goods-companion/?viewAsMember=true"
                 className="btn-sm bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] py-[5px] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%]"
               >
-                Register
+                Follow us!
               </Link>
             </li>
           </ul>
