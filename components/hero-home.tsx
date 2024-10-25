a184ffb23d6ce7b45564cac64ff084dea5c1e41b
margauxpages <39771534+margauxpages@users.noreply.github.com>
2024-10-25 14:17:22 -0500

    Update hero-home.tsx

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -13,7 +13,7 @@ export default function HeroHome() {
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
             >
-              Hazmats, Optimized
+              We digitize your hazardous inventory
             </h1>
             <div className="mx-auto max-w-3xl">
               <p
@@ -21,7 +21,7 @@ export default function HeroHome() {
                 data-aos="fade-up"
                 data-aos-delay={200}
               >
-                We digitize hazardous inventory, to increase their value and profitability. From gas cylinders to electric polls and chemical barrels, we make sure your hazmats are were they need to be, automatically.
+                We increase hazmats' value, safety and profitability. From gas cylinders to electric polls and chemical barrels, we make sure your hazmats are were they need to be, automatically.
               </p>
               <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
                 <div data-aos="fade-up" data-aos-delay={400}>
