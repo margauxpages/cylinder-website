bd51ab9fe3766adb28e123ebc021820b2b7a0638
margauxpages <39771534+margauxpages@users.noreply.github.com>
2024-10-26 11:39:47 -0500

    Update cta.tsx

--- components/cta.tsx
+++ components/cta.tsx
@@ -23,7 +23,7 @@ export default function Cta() {
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-8 font-nacelle text-3xl font-semibold text-transparent md:text-4xl"
               data-aos="fade-up"
             >
-              Join the content-first platform
+              Join the future of hazardous management
             </h2>
             <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
               <div data-aos="fade-up" data-aos-delay={400}>
