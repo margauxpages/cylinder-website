c8215863abce27f6c3d5ea08694ac41428d4980b
margauxpages <39771534+margauxpages@users.noreply.github.com>
2025-01-21 10:10:43 +0100

    Update hero-home.tsx

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -8,9 +8,6 @@ export default function HeroHome() {
         {/* Hero content */}
         <div className="py-12 md:py-20">
           {/* Section header */}
-          <p class="eyebrow">
-          <span class="eyebrow__part">🏆 A Techstars Company 2024</span>
-      </p>
           <div className="pb-12 text-center md:pb-20">
             <h1
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
